682c50b525fba2dfd5af4e54ba557c96485354c3
boseong <miraclesoul1121@naver.com>
2026-07-10 17:53:03 +0900

    inventory 추가작업

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import Medicine
 # Register your models here.
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "manufacturer",
+        "stock",
+        "minimum_stock",
+        "stock_status",
+        "updated_at",
+    )
+    search_fields = (
+        "name",
+        "manufacturer",
+    )
+    list_filter = (
+        "manufacturer",
+        "created_at",
+    )
+
+    @admin.display(description="재고 상태")
+    def stock_status(self, obj):
+        if obj.is_low_stock:
+            return "부족"
+        return "정상"
