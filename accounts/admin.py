9fc02eea9a75e57a71eb311bff97fa7d996e8d06
HighBuilder08 <code.shadow00@gmail.com>
2026-07-10 13:59:25 +0900

    feat: admin 페이지 수정

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import Pharmacy
-from .models import User
+from .models import Pharmacy, User
 
-admin.site.register(Pharmacy)
-admin.site.register(User)
+
+@admin.register(Pharmacy)
+class PharmacyAdmin(admin.ModelAdmin):
+    list_display = (
+        "pharmacy_name",
+        "business_name",
+        "owner_name",
+        "phone",
+        "status",
+    )
+
+    search_fields = (
+        "pharmacy_name",
+        "business_name",
+    )
+
+    list_filter = (
+        "status",
+    )
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = (
+        "username",
+        "name",
+        "role",
+        "pharmacy",
+        "is_approved",
+        "is_staff",
+    )
+
+    search_fields = (
+        "username",
+        "name",
+    )
+
+    list_filter = (
+        "role",
+        "is_approved",
+    )
--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'prescriptions.apps.PrescriptionsConfig'
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
